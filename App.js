8eea5ddaed68846f6da6000235e0dd24b39233ac
Quoc Anh Truong <quocanhkma1999@gmail.com>
2021-04-23 20:45:48 +0700

    init project

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'; 
+import Game from '../src/Game/Game';
+
+
+class App extends Component {
+    render(){
+        return <Game />
+    }
+}
+
+export default App;
+
+
+
+
